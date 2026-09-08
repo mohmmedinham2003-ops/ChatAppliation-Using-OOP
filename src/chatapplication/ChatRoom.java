7f12fec5cd30b5dc72e87a2320b46a8db0a40ac5
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-09-08 20:34:14 +0530

    message implementation

--- src/chatapplication/ChatRoom.java
+++ src/chatapplication/ChatRoom.java
@@ -10,11 +10,24 @@ import java.util.ArrayList;
  *
  * @author Inham
  */
-public class ChatRoom {
+class ChatRoom {
+
     private ArrayList<User> users = new ArrayList<>();
     private ArrayList<Message> messages = new ArrayList<>();
-    
-    public void addUser(User user){
-    users.add(user);
+
+    public void addUser(User user) {
+        users.add(user);
     }
+
+    public void sendMessage(User user, String text) {
+        Message message = new Message(user, text);
+        messages.add(message);
+    }
+    
+    public void displayMessage(){
+        for(Message message : messages){
+            message.displayMessage();
+        }
+    }
+    
 }
